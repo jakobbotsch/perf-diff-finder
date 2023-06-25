ebd27ed147dfa70004b53829c65cf090a9fa73ae
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2023-06-25 22:18:47 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace perf_diff_finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
